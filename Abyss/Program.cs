31179d14d74d36278e3b4137d60544f9367d4523
Яна Зайнуллина <yamina1105@gmail.com>
2025-02-08 18:29:18 +0300

    добавление метода

---
+++ Abyss/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abyss
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // ТЕХНИЧЕСКАЯ ЧАСТЬ: ОБЪЯВЛЕНИЕ ПЕРЕМННЫХ, НАПОЛНЕНИЕ МАССИВОВ, РАНДОМАЙЗЕР    
+            int room = 1; //количество пройденных комнат
+            int[] dungeonMap = new int[10];
+            string poison = "Восстановительное зелье", none = "---"; //заполнение инвентаря амуницией и пустотой
+            string[] inventory = new string[5] { poison, poison, poison, none, none };
+            int health = 100, gold = 30, arrow = 5; //HP
+            Random rnd = new Random();
+
+            // НАЧАЛО ИГРЫ
+            Console.WriteLine("Добро пожаловать в игру Подземелье!\nПройдите все 9 комнат, прежде чем сразиться с боссом в десятой комнате!");
+            
+
+            for (int i = 0; i < dungeonMap.Length; i++)
+            {
+                dungeonMap[i] = rnd.Next(1, 5); //заполнене испытаниями
+
+
+            }
+
+            /// <summary>
+            /// Метод мониторинга статистики пройденных уровней
+            /// </summary>
+            /// <param name="roomsCompleted"></param>
+        }
+            static void GameStatistic(int roomsCompleted)
+            {
+                int totalRooms = 10;
+                int roomsCount = totalRooms - roomsCompleted; // Осталось комнат
+                Console.WriteLine($"Комнат пройдено: {roomsCompleted}\nОсталось комнат: {roomsCount}");
+            }
+
+    }
+}
